ed0d4aa58766d1fad8a7f6699f7a28b3bbe5ffb5
Gerardo Juan Martin Serrano Gallego <serranogallegogerardowork@gmail.com>
2024-08-17 11:28:22 -0300

    fix - 01: adding support

--- getFirstEpisode.py
+++ getFirstEpisode.py
@@ -4,31 +4,41 @@ from requests_html import HTMLSession
 import subprocess
 
 if __name__ == "__main__":
-    # Obtener la lista de enlaces como argumentos de línea de comandos
+    # Check if links were provided as command-line arguments
+    if len(sys.argv) < 2:
+        print("Please provide links as command-line arguments.")
+        sys.exit(1)
 
-    #desarmar rompecabezas comentando esto
     links = sys.argv[1:]
-    
-    # Tu código para procesar los enlaces
+
     episode_links_list = []
     session = HTMLSession()
 
     for link in links:
-        r = session.get(link)
-        r.html.render()
-        episodes = r.html.find('.ListCaps li a')
+        # Check if the link is valid
+        if not link:
+            print(f"Skipping empty link.")
+            continue
 
-        if episodes:
-            first_episode = episodes[-1]
-            episode_link = first_episode.absolute_links.pop()  
-            episode_links_list.append(episode_link)
-            print("Enlace del primer episodio:", episode_link)
-        else:
-            print("No se encontraron episodios disponibles para", link)
+        try:
+            r = session.get(link)
+            r.html.render()
+            episodes = r.html.find('.ListCaps li a')
+        except Exception as e:
+            print(f"Error processing link {link}: {e}")
+            continue
+
+        # Check if any episodes were found
+        if not episodes:
+            print(f"No episodes found for link {link}")
+            continue
+
+        first_episode = episodes[-1]
+        episode_link = first_episode.absolute_links.pop()
+        episode_links_list.append(episode_link)
+        print(f"First episode link: {episode_link}")
 
     session.close()
-
-    # Pasar episode_links_list a otro script usando subprocess
 
     print(episode_links_list)
     subprocess.run(["python", "getAllLInks.py"] + episode_links_list)
